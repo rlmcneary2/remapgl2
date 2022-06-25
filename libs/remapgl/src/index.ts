589f87eae7598d93609ef1ff0a2b25158b1a0878
rlmcneary2 <rlmcneary2@gmail.com>
2022-06-25 15:03:03 -0400

    Add the MapPopup tp display a Popup directly on the Map.

--- libs/remapgl/src/index.ts
+++ libs/remapgl/src/index.ts
@@ -4,6 +4,7 @@ export * from "./lib/geolocate-control/geolocate-control";
 export * from "./lib/layer-collection/layer-collection";
 export * from "./lib/marker/marker";
 export * from "./lib/navigation-control/navigation-control";
+export * from "./lib/popup/map-popup";
 export * from "./lib/remapgl";
 export * from "./lib/attribution-control/attribution-control";
 export * from "./lib/scale-control/scale-control";
---
+++ libs/remapgl/src/lib/popup/map-popup.tsx
@@ -0,0 +1,56 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import {
+  LngLatLike,
+  Popup as PopupGL,
+  PopupOptions as PopupOptionsGL
+} from "mapbox-gl";
+import { useMapGL } from "../context";
+import { Popup } from "./popup";
+
+/**
+ * Display a Popup object directly on the Map. The children of this component
+ * are displayed as the Popup contents.
+ */
+export function MapPopup({
+  children,
+  lngLat,
+  options
+}: React.PropsWithChildren<Props>) {
+  const ref = useRef(null);
+  const popup = useRef<PopupGL>(null);
+  const { mapGL } = useMapGL();
+
+  useEffect(() => {
+    if (!ref || !lngLat || !!popup.current) {
+      return;
+    }
+
+    const nextPopup = new PopupGL(options ?? {})
+      .setDOMContent((ref as MutableRefObject<HTMLElement>).current)
+      .addTo(mapGL);
+
+    popup.current = nextPopup;
+
+    return () => {
+      nextPopup.remove();
+      popup.current = null;
+    };
+  }, [lngLat, mapGL, options]);
+
+  useEffect(() => {
+    if (!popup.current) {
+      return;
+    }
+
+    lngLat && popup.current.setLngLat(lngLat);
+  }, [lngLat, popup]);
+
+  return <Popup ref={ref}>{children}</Popup>;
+}
+
+interface Props {
+  /** The location of the popup on the map. */
+  lngLat: LngLatLike;
+  /** Options that affect the display of the map. */
+  options?: PopupOptionsGL;
+}
