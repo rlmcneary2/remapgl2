ec3674f4913ea93e6cdf6f0ba64ef1a1e2f34cd7
rlmcneary2 <rlmcneary2@gmail.com>
2022-06-25 13:19:43 -0400

    Export useMapGL.

--- libs/remapgl/src/index.ts
+++ libs/remapgl/src/index.ts
@@ -1,3 +1,5 @@
+import { useMapGL as useMapGLInternal } from "./lib/context";
+
 export * from "./lib/geolocate-control/geolocate-control";
 export * from "./lib/layer-collection/layer-collection";
 export * from "./lib/marker/marker";
@@ -7,3 +9,13 @@ export * from "./lib/attribution-control/attribution-control";
 export * from "./lib/scale-control/scale-control";
 export * from "./lib/fullscreen-control/fullscreen-container";
 export * from "./lib/types";
+
+/**
+ * Provides access the Mapbox Map instance.
+ */
+const useMapGL: () => Pick<
+  ReturnType<typeof useMapGLInternal>,
+  "mapGL" | "ready"
+> = useMapGLInternal;
+
+export { useMapGL };
