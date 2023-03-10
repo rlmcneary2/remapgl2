593601bbab9348d22b25d079536eff02e12ac99f
rlmcneary2 <rich@bitovi.com>
2023-03-09 20:33:29 -0700

    Update NX from 13 to 14.

--- apps/test-app/jest.config.ts
+++ apps/test-app/jest.config.ts
@@ -1,12 +1,14 @@
-module.exports = {
-  displayName: "remapgl",
+/* eslint-disable */
+export default {
+  displayName: "test-app",
   preset: "../../jest.preset.js",
   transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
     "^.+\\.[tj]sx?$": [
       "babel-jest",
       { cwd: __dirname, configFile: "./babel-jest.config.json" }
     ]
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/libs/remapgl"
+  coverageDirectory: "../../coverage/apps/test-app"
 };
---
+++ libs/remapgl/jest.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default {
+  displayName: "remapgl",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "babel-jest",
+      { cwd: __dirname, configFile: "./babel-jest.config.json" }
+    ]
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/libs/remapgl"
+};
